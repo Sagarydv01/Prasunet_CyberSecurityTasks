5ccfb6e46bdc5290fcc6aefb5b19312ced7ea905
Sagarydv01 <sagydv01@gmail.com>
2024-07-15 09:59:26 +0530

    4.4 Commit - Task-03 Code updated by adding sequencial password checking to the program!

--- Prasunet_CS_03/Password Complexity Checker.py
+++ Prasunet_CS_03/Password Complexity Checker.py
@@ -8,6 +8,15 @@ import tkinter as tk
 from tkinter import *
 import string
 
+def is_sequential(password):
+    sequences = [string.ascii_lowercase, string.ascii_uppercase, string.digits]
+    for seq in sequences:
+        for i in range(len(seq) - 2):
+            sub_seq = seq[i:i + 3]
+            if sub_seq in password or sub_seq[::-1] in password:
+                return True
+    return False
+
 def check_password():
     password = password_entry.get()
 
@@ -39,13 +48,17 @@ def check_password():
     else:
         remarks.append("Include at least one special character.")
 
+    if is_sequential(password):
+        remarks.append("Password should not contain sequences of numbers or letters (e.g., '123', 'abc').")
+        strength = min(strength, 2)  # Reduce strength to weak if sequence is found
+
     if strength == 5:
         result_text.set("Very Strong Password")
-    elif strength >= 3:
+    elif strength == 4:
         result_text.set("Strong Password")
-    elif strength == 2:
+    elif strength == 3:
         result_text.set("Moderate Password")
-    elif strength == 1:
+    elif strength == 2:
         result_text.set("Weak Password")
     else:
         result_text.set("Very Weak Password")
